63d8aeef338bc8a29da5191f0bf184b787eccc78
piyush0049 <piyushjoshi81204@gmail.com>
2025-08-19 18:29:05 +0530

    authorization

--- index.js
+++ index.js
@@ -31,17 +31,21 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",      
+      "http://localhost:5173",
       "https://ecommerce-frontend-web-ten.vercel.app",
       "https://main--dulcet-conkies-e3a8f0.netlify.app",
       "http://localhost:3000",
       "https://main--ecommercefrontend123654.netlify.app",
     ],
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+// Handle preflight requests
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
